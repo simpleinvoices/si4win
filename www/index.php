28651e5fbb50995ea5f04196e87cb51a71f77c16
John Mclaren <john@johnscs.com>
2013-05-05 15:14:56 -0400

    Cleanup

---
+++ www/index.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>Simple Invoices For Windows</title>
+</head>
+<body>
+	<p>Simple invoices needs to be extracted to the /www folder of this portable web server application.</p>
+</body>
+</html>
